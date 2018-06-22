5e10600901c95f18d02cf0f38597efb45c8f1401
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-06-22 15:26:19 +0200

    FEAT: simple routing

--- code-split/src/App.js
+++ code-split/src/App.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			route: 'page1'
+		};
+	}
+
+	onRouteChange = route => {
+		this.setState({ route });
+	}
+
 	render() {
+		const { route } = this.state;
+
 		return (
-			<div className="app">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-        		</p>
-			</div>
+			<Fragment>
+				{route === 'page1' &&
+					<Page1 onRouteChange={this.onRouteChange} />
+				}
+				{route === 'page2' &&
+					<Page2 onRouteChange={this.onRouteChange} />
+				}
+				{route === 'page3' &&
+					<Page3 onRouteChange={this.onRouteChange} />
+				}
+			</Fragment>
 		);
 	}
 }
---
+++ code-split/src/components/Page2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Page2 = ({ onRouteChange }) => {
+    return (
+        <section className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <button
+                onClick={() => onRouteChange('page1')}>Page 1</button>
+            <button className="disabled">Page 2</button>
+            <button
+                onClick={() => onRouteChange('page3')}>Page 3</button>
+        </section>
+    )
+}
+
+export default Page2;
