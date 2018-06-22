8fac517dde76c6a990e2ad8024dea4ece84527f2
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-06-22 15:06:25 +0200

    Initial commit

---
+++ code-split/src/components/Page1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Page1 = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <button onClick={}>Page 2</button>
+            <button onClick={}>Page 3</button>
+        </div>
+    )
+}
