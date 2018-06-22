ad2a9c1310735d0f4415ea6f3870c979ff7ff352
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-06-22 16:12:13 +0200

    FEAT: implement AsyncComponent

--- code-split/src/App.js
+++ code-split/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Page1 from './components/Page1';
 import React, { Component } from 'react';
+import AsyncComponent from './components/AsyncComponent';
 
 class App extends Component {
 	constructor() {
@@ -8,20 +9,12 @@ class App extends Component {
 
 		this.state = {
 			route: 'page1',
-			component: ''
+			component: null
 		};
 	}
 
-	onRouteChange = async route => {
-		if (route === 'page1') {
-			this.setState({ route: route });
-		} else if (route === 'page2') {
-			const Page2 = await import('./components/Page2');
-			this.setState({ route: route, component: Page2.default });
-		} else if (route === 'page3') {
-			const Page3 = await import('./components/Page3');
-			this.setState({ route: route, component: Page3.default });
-		}
+	onRouteChange = route => {
+		this.setState({ route });
 	}
 
 	render() {
@@ -29,8 +22,12 @@ class App extends Component {
 
 		if (route === 'page1') {
 			return <Page1 onRouteChange={this.onRouteChange} />
-		} else {
-			return <this.state.component onRouteChange={this.onRouteChange} />
+		} else if (route === 'page2') {
+			const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+			return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+		} else if (route === 'page3') {
+			const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+			return <AsyncPage3 onRouteChange={this.onRouteChange} />;
 		}
 	}
 }
